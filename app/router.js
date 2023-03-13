ebf8245aa79a5d15bdf4fe02d6f2c82cea771b45
Noah Branch <noah.branch@gmail.com>
2023-03-13 15:12:54 -0400

    Initial commit and copying

--- app/router.js
+++ app/router.js
@@ -6,4 +6,9 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+	this.route('about');
+	this.route('dungeon-master');
+	this.route('player');
+	this.route('characters');
+});
